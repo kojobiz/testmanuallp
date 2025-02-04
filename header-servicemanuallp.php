7150748660fe3646c9e6e449276d52a75da37192
小城 <kojolumi@gamil.com>
2025-02-04 18:35:07 +0900

    t3est

---
+++ header-servicemanuallp.php
@@ -0,0 +1,161 @@
+<?php
+
+/**
+ * The header for our theme servicemanuallp
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package ikkosha_theme
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+
+<head>
+	<meta charset="<?php bloginfo('charset'); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+
+	<style>
+		:root {
+			--base-font-color: #333;
+			--bg-color: #fff;
+			--main-width: 120.0rem;
+			--primary-color: #f0353c;
+			/* Swiper Setting */
+			--swiper-pagination-bullet-height: 1.2rem;
+			--swiper-pagination-bullet-horizontal-gap: .6rem;
+			--swiper-pagination-bullet-width: 1.2rem;
+			--swiper-pagination-color: #f0353c;
+			--swiper-theme-color: #f0353c;
+		}
+
+		[data-color="black"] {
+			--txt-color: #000;
+		}
+
+		[data-color="red"] {
+			--txt-color: #d6000f;
+		}
+
+		[data-color="white"] {
+			--txt-color: #fff;
+		}
+
+		.lp-logosize {
+			max-width: 75%;
+		}
+
+		.lp-navi-menu-ul li {
+			text-wrap: nowrap !important;
+		}
+
+		/* .lp-navi-menu-ul li:not(:last-child) {
+		margin-right: 15px!important;
+	} */
+	</style>
+
+	<!-- Google Fonts -->
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Noto+Serif+JP:wght@400;600&family=Playfair+Display&display=swap" rel="stylesheet">
+	<link href='https://fonts.googleapis.com/css?family=Righteous' rel='stylesheet'>
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/assets/sass/pages/smlp.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
+	<link rel="shortcut icon" href="<?php echo get_template_directory_uri() ?>/favicon.ico">
+
+	<?php wp_head(); ?>
+</head>
+
+<?php
+if (is_home() && is_front_page()) {
+	$custom_body_class = 'home';
+} else {
+	$custom_body_class = 'lower';
+}
+?>
+
+<body <?php body_class($custom_body_class); ?>>
+
+	<?php wp_body_open(); ?>
+
+	<header id="header" class="site-header wh">
+		<div class="header-inner">
+			<h1 class="header-title">
+				<a href="<?php echo esc_url(home_url('/service')); ?>/exhibition_support/lp2">
+					<figure class="logo lp-logosize">
+						<img src="<?php echo get_template_directory_uri() ?>/images/common/logo.svg" width="216" height="46" alt="株式会社一向社" class="logo01 lp-logosize">
+						<img src="<?php echo get_template_directory_uri() ?>/images/common/logo_100th.svg" width="70" height="auto" alt="100th ANNIVERSARY" class="logo02 lp-logosize">
+					</figure>
+				</a>
+			</h1>
+
+			<nav class="header-navi">
+				<div class="navi-phone">
+					<a href="tel:0120150585" class="txt-serif"><span>0120-150-<span class="lastnum-tel">585</span></span></a>
+					<span class="label">受付時間 / 平日 9:00～18:00</span>
+				</div>
+				<ul class="navi-menu txt-serif lp-navi-menu-ul">
+					<!-- navi -->
+					<li><a href="<?php echo esc_url(home_url('/service')); ?>/exhibition_support/lp2#last-achvm-intro">実績紹介</a></li>
+					<li><a href="<?php echo esc_url(home_url('/service')); ?>/exhibition_support/lp2#service-flow">サービス詳細</a></li>
+					<li><a href="<?php echo esc_url(home_url('/service')); ?>/exhibition_support/lp2#service-comparison">他社サービスとの比較</a></li>
+					<li><a href="<?php echo esc_url(home_url('/service')); ?>/exhibition_support/lp2#service-faq">よくあるご質問</a></li>
+					<li class="btn-contact" data-color="white">
+						<a href="<?php echo esc_url(home_url('/service')); ?>/exhibition_support/lp2#contact">
+							<figure class="icon">
+								<img src="<?php echo get_template_directory_uri() ?>/images/common/icon_mail.png" width="31" height="24" alt="MAIL">
+								<figcaption>お問い合わせ</figcaption>
+							</figure>
+						</a>
+					</li>
+				</ul>
+			</nav>
+			<!-- sp menu -->
+			<div id="hamburgerMenu" class="hamburger-menu">
+				<div>
+					<span></span>
+					<span></span>
+					<span></span>
+				</div>
+			</div>
+		</div>
+	</header>
+
+	<div id="spNavi" class="tabOnly">
+		<ul class="navi-menu txt-serif">
+			<li>
+				<a href="<?php echo esc_url(home_url('/service')); ?>/exhibition_support/lp2">ホーム</a>
+			</li>
+			<li>
+				<a href="<?php echo esc_url(home_url('/service')); ?>/exhibition_support/lp2#last-achvm-intro">実績紹介</a>
+			</li>
+			<li>
+				<a href="<?php echo esc_url(home_url('/service')); ?>/exhibition_support/lp2#service-flow">サービス詳細</a>
+			</li>
+			<li>
+				<a href="<?php echo esc_url(home_url('/service')); ?>/exhibition_support/lp2#service-comparison">他社サービスとの比較</a>
+			</li>
+			<li>
+				<a href="<?php echo esc_url(home_url('/service')); ?>/exhibition_support/lp2#service-faq">よくあるご質問</a>
+			</li>
+			<li>
+				<a href="<?php echo esc_url(home_url('/privacy-policy-lp2')); ?>">プライバシーポリシー</a>
+			</li>
+			<li class="navi-contact">
+				<div class="navi-phone">
+					<a href="tel:0120150585" class="txt-serif"><span>0120-150-<span class="lastnum-tel">585</span></span></a>
+					<span class="label">受付時間 / 平日 9:00～18:00</span>
+				</div>
+				<a href="<?php echo esc_url(home_url('/service')); ?>/exhibition_support/lp2#contact" class="btn">お問い合わせ</a>
+			</li>
+		</ul>
+	</div>
+
+	
+
+	<main class="site-container">
