d9bad8008d05b5e92ff63520c1b70c3868440b30
lumijoe <spacedesign63@gmail.com>
2025-02-21 00:01:06 +0900

    test

--- page-lp1.php
+++ page-lp1.php
@@ -14,18 +14,18 @@ get_header('servicemanuallp');
             <section class="lp-white section100">
                 <div class="l-hero">
                     <div class="l-hero-wrapper">
-                        <div class="img-left"><img src="../../../cms/wp-content/themes/ikkosha_theme/images/service/manual/lp/img_hero_manual.png" alt="" width="343" style="max-width: 100%;"></div>
+                        <div class="img-left" id="heading05"><img src="../../../cms/wp-content/themes/ikkosha_theme/images/service/manual/lp/img_hero_manual.png" alt="" width="343" style="max-width: 100%;"></div>
                         <div></div>
-                        <div class="img-right"><img src="../../../cms/wp-content/themes/ikkosha_theme/images/service/manual/lp/mv2.svg" alt="" height="445" style="max-width: 100%;"></div>
+                        <div class="img-right" id="heading04"><img src="../../../cms/wp-content/themes/ikkosha_theme/images/service/manual/lp/mv2.svg" alt="" height="445" style="max-width: 100%;"></div>
                         <!-- <div><img src="./images/service/manual/lp/mv2.svg" alt="" height="530" style="margin-top: -26.5px;"></div> -->
                     </div>
                     <div class="l-hero-wrapper--center">
                         <h1><img src="../../../cms/wp-content/themes/ikkosha_theme/images/service/manual/lp/img_title.png" alt="" style="max-width: 100%; max-height: 212px;"></h1>
                         <p class="p-hero-cap">制作の進行管理からリーガルチェックまでプロにご相談ください</p>
                         <ul class="l-featurewrapper">
-                            <li><img src="../../../cms/wp-content/themes/ikkosha_theme/images/service/manual/lp/icon_hero01.png" alt="" width="158" style="max-width: 100%;"></li>
-                            <li><img src="../../../cms/wp-content/themes/ikkosha_theme/images/service/manual/lp/icon_hero02.png" alt="" width="158" style="max-width: 100%;"></li>
-                            <li><img src="../../../cms/wp-content/themes/ikkosha_theme/images/service/manual/lp/icon_hero03.png" alt="" width="158" style="max-width: 100%;"></li>
+                            <li><img src="../../../cms/wp-content/themes/ikkosha_theme/images/service/manual/lp/icon_hero01.png" alt="" width="158" style="max-width: 100%;" id="heading01"></li>
+                            <li><img src="../../../cms/wp-content/themes/ikkosha_theme/images/service/manual/lp/icon_hero02.png" alt="" width="158" style="max-width: 100%;" id="heading02"></li>
+                            <li><img src="../../../cms/wp-content/themes/ikkosha_theme/images/service/manual/lp/icon_hero03.png" alt="" width="158" style="max-width: 100%;" id="heading03"></li>
                         </ul>
                     </div>
                     <a href="./#contact">
@@ -736,7 +736,44 @@ get_header('servicemanuallp');
         </div>  
     </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
- </body>
+    <script>
+    document.addEventListener('DOMContentLoaded', () => {
+        const headings = [
+            { id: 'heading01', duration: 2000, easing: 'ease' },
+            { id: 'heading02', duration: 2500, easing: 'ease' },
+            { id: 'heading03', duration: 3000, easing: 'ease' },
+            { id: 'heading04', duration: 1500, easing: 'ease-in' }, 
+            { id: 'heading05', duration: 1500, easing: 'ease-in' }, 
+        ];
+
+        headings.forEach(({ id, duration, easing }) => {
+            const heading = document.querySelector(`#${id}`);
+            let keyframes;
+            if (id === 'heading04') {
+                keyframes = {
+                    opacity: [0, 1],
+                    translate: ['100px', '0'], // 右からシュッと現れる
+                };
+            } else if (id === 'heading05') {
+                keyframes = {
+                    opacity: [0, 1],
+                    translate: ['-100px', '0'], // 左からシュッと現れる
+                };
+            } else {
+                keyframes = {
+                    opacity: [0, 1],
+                    translate: ['0 80px', 0],
+                };
+            }
+            const options = {
+                duration: duration,
+                easing: easing,
+            };
+            heading.animate(keyframes, options);
+        });
+    });
+</script>
+</body>
  </html>
 <?php
 get_footer('servicemanuallp');
